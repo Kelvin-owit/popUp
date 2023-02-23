c02b3a1fe3088955dd291a2e56fd6cb27baf704e
Kelvin-owit <owitkelvin@gmail.com>
2023-02-23 14:48:30 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var popup = document.getElementById("popup");
+
+function openPopup() {
+  popup.classList.add("open-Popup");
+}
+
+function closePopup() {
+  popup.classList.remove("open-Popup");
+}
